cdf4fce9010112e392efcd294faa0e5ac33b7336
Alexander <a.biryukov164@gmail.com>
2024-03-12 15:19:21 +0400

    add method cast_to_object_list in Vacancy

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -1,33 +1,48 @@
-import json
-
-
 class Vacancy:
-    """Класс для представления вакансий"""
+    """ Класс для представления вакансий """
+    name: str
+    url: str
+    salary: dict
+    requirement: str
+    date: str
+    area: str
 
-    def __init__(self, name: str, url: str,  salary: dict, requirement: str, data: str):
+    def __init__(self, name: str, url: str,  salary: dict, requirement: str, date: str, area: str):
+        """
+        Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
+        :param name: Название вакансии
+        :param url: Ссылка на вакансию
+        :param salary: Зарплата
+        :param requirement: Требования
+        :param date: Дата публикации
+        :param area: Страна, область или город
+        """
         self.name = name
         self.url = url
         self.salary = salary
         self.requirement = requirement
-        self.data = data
+        self.date = date
+        self.area = area
 
     @classmethod
-    def cast_to_object_list(cls):
-        """Преобразование набора данных из JSON в список объектов"""
-
-        with open("vacancies.json", encoding="utf-8") as f:
-            data = json.load(f)
-
+    def cast_to_object_list(cls, data: list) -> list:
+        """
+        Преобразует набор данных из JSON в список объектов
+        :param data: Список с данными о вакансиях
+        :return: Список объектов класса Vacancy
+        """
         vacancy_objects = []
 
         for vacancy in data:
             name = vacancy.get("name")
             url = vacancy.get("alternate_url")
+            del vacancy.get("salary")["gross"]
             salary = vacancy.get("salary")
             requirement = vacancy.get("snippet").get("requirement")
-            data = vacancy.get("published_at")
+            date = vacancy.get("published_at")
+            area = vacancy.get("area").get("name")
 
-            vacancy_object = cls(name, url, salary, requirement, data)
+            vacancy_object = cls(name, url, salary, requirement, date, area)
             vacancy_objects.append(vacancy_object)
 
         return vacancy_objects
