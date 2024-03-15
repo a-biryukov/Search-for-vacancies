3a1be7873b8280a6c9f1bdee7707cad5db8dfbd3
Alexander <a.biryukov164@gmail.com>
2024-03-15 19:03:48 +0400

    add class ExchangeRates

---
+++ src/class_exchange_rates.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class ExchangeRates:
+    """ Класс для получения курсов валют """
+
+    def get_exchange_rates(self):
+        """
+        Фильтрует данные, отбирает код валюты и курс
+        :return: Словарь с курсами валют
+        """
+        data = self.__get_data()
+
+        exchange_rates = {}
+
+        for item in data:
+            currency = item.text.split("\n")[10:]
+
+            for num in range(len(currency) // 7):
+                del currency[0]
+                currency_code = currency.pop(0)
+                quantity = int(currency.pop(0))
+                del currency[0]
+                exchange_rate = float(currency.pop(0).replace(",", ".")) / quantity
+                del currency[0]
+                del currency[0]
+                exchange_rates[currency_code] = exchange_rate
+
+        return exchange_rates
+
+    @staticmethod
+    def __get_data():
+        """ Получает данные с сайта ЦБ РФ """
+
+        url = "https://cbr.ru/currency_base/daily/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        data = soup.find_all("table", class_="data")
+
+        return data
