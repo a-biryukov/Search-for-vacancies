b66ec59b3da4c3235cb62e1318623d6032498aa3
Alexander <a.biryukov164@gmail.com>
2024-03-18 14:27:23 +0400

    add comparison methods, add method str, change cast_to_object_lict

--- src/class_vacancy.py
+++ src/class_vacancy.py
@@ -1,50 +1,107 @@
+from datetime import date
+
+
 class Vacancy:
     """ Класс для представления вакансий """
     name: str
     url: str
     salary: dict
-    requirement: str
-    date: str
+    salary_indicated: bool
+    snippet: dict
+    publication_date: str
     area: str
 
     number_of_vacancies = 0
 
-    def __init__(self, name: str, url: str,  salary: dict, requirement: str, date: str, area: str):
+    def __init__(self, name: str, url: str,  salary: dict, snippet: dict, publication_date: str, area: str) -> None:
         """
         Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра.
         :param name: Название вакансии
         :param url: Ссылка на вакансию
         :param salary: Зарплата
-        :param requirement: Требования
-        :param date: Дата публикации
+        :param snippet: Требования и обязанности
+        :param publication_date: Дата публикации
         :param area: Страна, область или город
         """
         self.name = name
         self.url = url
         self.salary = salary
-        self.requirement = requirement
-        self.date = date
+        self.salary_indicated = bool(salary)
+        self.snippet = snippet
+        self.publication_date = publication_date
         self.area = area
 
         Vacancy.number_of_vacancies += 1
 
-    def __lt__(self, other):
+    def __str__(self):
+        """
+        :return: Строка с информацией о вакансии
+        """
+        salary_list = []
+        if self.salary_indicated:
+            if self.salary.get("from"):
+                salary_from = self.salary.get("from")
+                salary_from_str = f"от {salary_from}"
+                salary_list.append(salary_from_str)
+            if self.salary.get("to"):
+                salary_to = self.salary.get("to")
+                salary_to_str = f"до {salary_to}"
+                salary_list.append(salary_to_str)
+        else:
+            salary_list.append("не указана")
+
+        salary = " ".join(salary_list)
+
+        return f"""Вакансия: {self.name}
+{self.area}
+Требования: {self.snippet.get("requirement")}
+Обязанности: {self.snippet.get("responsibility")}
+Зарплата {salary}
+Дата публикации {self.publication_date}
+Ссылка на вакансию: {self.url}
+"""
+
+    def __lt__(self, other) -> bool:
         """ Метод < (меньше) для сравнения двух вакансий  """
-        other_verified = self.__verify_data(other)
-        self_salary, other_salary = self.__get_salary_for_comparison(other_verified)
-        return self_salary < other_salary
+        try:
+            other_verified = self.__verify_data(other)
+        except TypeError as te:
+            print(te)
+        else:
+            try:
+                self_salary, other_salary = self.__get_salary_for_comparison(other_verified)
+            except ValueError as ve:
+                print(ve)
+            else:
+                return self_salary < other_salary
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         """ Метод <= (меньше либо равно) для сравнения двух вакансий  """
-        other_verified = self.__verify_data(other)
-        self_salary, other_salary = self.__get_salary_for_comparison(other_verified)
-        return self_salary <= other_salary
+        try:
+            other_verified = self.__verify_data(other)
+        except TypeError as te:
+            print(te)
+        else:
+            try:
+                self_salary, other_salary = self.__get_salary_for_comparison(other_verified)
+            except ValueError as ve:
+                print(ve)
+            else:
+                return self_salary <= other_salary
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         """ Метод == (равно) для сравнения двух вакансий  """
-        other_verified = self.__verify_data(other)
-        self_salary, other_salary = self.__get_salary_for_comparison(other_verified)
-        return self_salary == other_salary
+        try:
+            other_verified = self.__verify_data(other)
+        except TypeError as te:
+            print(te)
+        else:
+            try:
+                self_salary, other_salary = self.__get_salary_for_comparison(other_verified)
+            except ValueError as ve:
+                print(ve)
+            else:
+                return self_salary == other_salary
 
     @classmethod
     def cast_to_object_list(cls, vacancy_list: list, currency_rates: dict) -> list:
@@ -59,63 +116,94 @@ class Vacancy:
         for vacancy in vacancy_list:
             name = vacancy.get("name")
             url = vacancy.get("alternate_url")
-            date = vacancy.get("published_at")
+            the_date = date.fromisoformat(vacancy.get("published_at").split("T")[0])
+            publication_date = date.strftime(the_date, "%d.%m.%Y")
             area = vacancy.get("area").get("name")
-            req = vacancy.get("snippet").get("requirement")
+            snippet = vacancy.get("snippet")
+            for item in ["requirement", "responsibility"]:
+                try:
+                    snippet[item] = snippet.get(item).replace("<highlighttext>", "").replace("</highlighttext>", "")
+                except AttributeError:
+                    snippet[item] = "не указаны"
 
-            try:
-                requirement = req.replace("<highlighttext>", "").replace("</highlighttext>", "")
-            except AttributeError:
-                requirement = "Требования не указаны"
+            salary = cls.__get_salary_info(vacancy, currency_rates)
 
-            if vacancy.get("salary"):
-                # Получаем зарплату и переводим её в рубли, если она указана в другой валюте
-                if vacancy.get("salary").get("currency") != "RUR":
-                    currency_rate = currency_rates.get(vacancy.get("salary").get("currency"))
-
-                    salary = {}
-                    if vacancy.get("salary").get("from"):
-                        salary_from = vacancy.get("salary").get("from") * currency_rate
-                        salary["from"] = round(salary_from)
-                    if vacancy.get("salary").get("to"):
-                        salary_to = vacancy.get("salary").get("to") * currency_rate
-                        salary["to"] = round(salary_to)
-                else:
-                    salary = {}
-                    if vacancy.get("salary").get("from"):
-                        salary_from = vacancy.get("salary").get("from")
-                        salary["from"] = salary_from
-                    if vacancy.get("salary").get("to"):
-                        salary_to = vacancy.get("salary").get("to")
-                        salary["to"] = salary_to
-
-                vacancy_object = cls(name, url, salary, requirement, date, area)
-                vacancy_objects.append(vacancy_object)
-
-            else:
-                salary = {"to": 0}
-
-                vacancy_object = cls(name, url, salary, requirement, date, area)
-                vacancy_objects.append(vacancy_object)
+            vacancy_object = cls(name, url, salary, snippet, publication_date, area)
+            vacancy_objects.append(vacancy_object)
 
         return vacancy_objects
 
     def __get_salary_for_comparison(self, other) -> tuple:
+        """
+        Получение зарплаты из объектов класса Vacancy для сравнения
+        :param other: Объект класса Vacancy
+        :return: Кортеж с данными о зарплате типа int объектов класса Vacancy
+        """
+        if not self.salary_indicated:
+            raise ValueError("В вакансии слева не указана зарплата")
+        if not other.salary_indicated:
+            raise ValueError("В вакансии справа не указана зарплата")
+
         self_salary_set = set(self.salary)
         other_salary_set = set(other.salary)
         intersection = list(self_salary_set & other_salary_set)
+
         if len(intersection) == 1:
             self_salary = self.salary.get(intersection[0])
             other_salary = other.salary.get(intersection[0])
-        else:
+            return self_salary, other_salary
+
+        elif len(intersection) == 2:
             self_salary = (self.salary.get("to") + self.salary.get("from")) // 2
             other_salary = (other.salary.get("to") + other.salary.get("from")) // 2
+            return self_salary, other_salary
 
-        return self_salary, other_salary
+        elif len(intersection) == 0:
+            raise ValueError("Вакансии нельзя сравнить, в одной указано зарплата 'от', а в другой 'до'")
 
     @classmethod
     def __verify_data(cls, other):
+        """
+        Проверка на принадлежность к классу Vacancy
+        :return: объект класса Vacancy
+        """
         if not isinstance(other, cls):
-            raise TypeError("Операнд справа должен иметь тип int или быть объектом класса Vacancy")
+            raise TypeError("Операнд справа должен быть объектом класса Vacancy")
+        else:
+            return other
 
-        return other
+    @staticmethod
+    def __get_salary_info(vacancy: dict, currency_rates: dict) -> dict:
+        """
+        Получение информации о зарплате из словаря с информацией о вакансии,
+        переводит зарплату в рубли, если она указана в другой валюте
+        :param vacancy: Словарь с информацией о вакансии
+        :param currency_rates: Словарь с курсами валют
+        :return: Словарь с информацией о зарплате
+        """
+        salary_dict = vacancy.get("salary")
+
+        if salary_dict:
+            salary = {}
+            currency_rate = currency_rates.get(salary_dict.get("currency"))
+
+            if salary_dict.get("currency") != "RUR" and currency_rate:
+                if salary_dict.get("from"):
+                    salary_from = salary_dict.get("from") * currency_rate
+                    salary["from"] = round(salary_from)
+                if salary_dict.get("to"):
+                    salary_to = salary_dict.get("to") * currency_rate
+                    salary["to"] = round(salary_to)
+            elif salary_dict.get("currency") != "RUR" and not currency_rate:
+                salary = None
+            else:
+                if salary_dict.get("from"):
+                    salary_from = salary_dict.get("from")
+                    salary["from"] = salary_from
+                if salary_dict.get("to"):
+                    salary_to = salary_dict.get("to")
+                    salary["to"] = salary_to
+        else:
+            salary = None
+
+        return salary
